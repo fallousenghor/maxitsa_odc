7b750283446f8d249e1e34d43c3c47cd1fb85472
fallousenghor <fallousenghor@gmail.com>
2025-07-25 15:40:53 +0000

    ok

--- public/index.php
+++ public/index.php
@@ -23,18 +23,5 @@ $router->dispatch($_SERVER['REQUEST_URI']);
 
 
 
-$host = getenv("DB_HOST");
-$db   = getenv("DB_NAME");
-$user = getenv("DB_USER");
-$pass = getenv("DB_PASSWORD");
-$port = getenv("DB_PORT");
 
-$dsn = "pgsql:host=$host;port=$port;dbname=$db";
 
-try {
-    $pdo = new PDO($dsn, $user, $pass);
-    echo "Connexion réussie à la base PostgreSQL Render !";
-} catch (PDOException $e) {
-    echo "Erreur de connexion : " . $e->getMessage();
-}
-?>
